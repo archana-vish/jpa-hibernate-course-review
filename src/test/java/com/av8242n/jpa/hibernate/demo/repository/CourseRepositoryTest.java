2219e80de4d035b65d98e87900feb5e3f9790367
Archana Viseswaran <A.Visveswaran@greenwich.ac.uk>
2019-06-22 07:29:08 +0100

    Added junit for findById

---
+++ src/test/java/com/av8242n/jpa/hibernate/demo/repository/CourseRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.av8242n.jpa.hibernate.demo.repository;
+
+import com.av8242n.jpa.hibernate.demo.DemoApplication;
+import com.av8242n.jpa.hibernate.demo.entity.Course;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= DemoApplication.class)
+public class CourseRepositoryTest {
+
+    @Autowired
+    CourseRepository repository;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Test
+    public void findById() {
+        logger.info("Test is running...");
+        Course course = repository.findById(10001);
+        assertEquals("JPA in 100 steps", course.getName());
+    }
+}
