b4f260af4fbb45741aa184a6f8783dc0fc476cf3
Archana Viseswaran <A.Visveswaran@greenwich.ac.uk>
2019-07-21 06:43:56 +0100

    Use predicates

--- src/test/java/com/av8242n/jpa/hibernate/demo/repository/CriteriaQueryTest.java
+++ src/test/java/com/av8242n/jpa/hibernate/demo/repository/CriteriaQueryTest.java
@@ -16,6 +16,7 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -30,7 +31,7 @@ public class CriteriaQueryTest {
 
 
     @Test
-    public void jpql_basic() {
+    public void all_courses_cq_basic() {
 
         // select c from course c
 
@@ -44,6 +45,34 @@ public class CriteriaQueryTest {
 
         // 3. Define Predicates etc using Criteria Builder
         // 4. Add Predicates etc to the Criteria Query
+
+        // 5. Build the TypedQuery using entity manager and criteria query
+        TypedQuery<Course> select_c_from_course_c =
+                entityManager.createQuery(cq.select(courseRoot));
+        List resultList = select_c_from_course_c.getResultList();
+        logger.info("Value is {} ", resultList);
+
+    }
+
+    @Test
+    public void courses_cq_basic_where_100steps() {
+
+        // select c from course c
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Predicate like_100_steps = cb.like(courseRoot.get("name"), "%100 steps");
+
+
+        // 4. Add Predicates etc to the Criteria Query
+        cq.where(like_100_steps);
 
         // 5. Build the TypedQuery using entity manager and criteria query
         TypedQuery<Course> select_c_from_course_c =
